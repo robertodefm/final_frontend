24303de4f456a3f71ff0b46cc9af6f3d36d8556b
Roberto Moniz <robertodefm@gmail.com>
2023-05-19 15:13:30 +0100

    Search OK, working in Cocktail

--- src/views/CommentsView.vue
+++ src/views/CommentsView.vue
@@ -1,3 +1,83 @@
 <template>
-    
+
+<section class="color">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-10 col-xl-8 text-center">
+        <h3 class="mb-4">Testimonials</h3>
+        <p class="mb-4 pb-2 mb-md-5 pb-md-0">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet
+          numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum
+          quisquam eum porro a pariatur veniam.
+        </p>
+      </div>
+    </div>
+  
+    <div class="row text-center">
+      <div class="col-md-6 mb-4 mb-md-0 mt-4 ">
+        <div class="d-flex justify-content-center mb-4">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(22).webp"
+            class="rounded-circle shadow-1-strong" width="100" height="100" />
+        </div>
+        <p class="lead my-3 ">
+            Excellent Old Fashioned and the most delicious Truffle Honey Butter Potato Chips.  Only 5 small plates on the menu, but SUPER YUMMY and a nice variety.
+        </p>
+        <p class="font-italic font-weight-normal mb-0">- Anna Morian</p>
+      </div>
+      <div class="col-md-6 mb-0 mt-4">
+        <div class="d-flex justify-content-center mb-4">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp"
+            class="rounded-circle shadow-1-strong" width="100" height="100" />
+        </div>
+        <p class="lead my-3 ">
+          "Neque cupiditate assumenda in maiores repudiandae mollitia architecto elit sed
+          adipiscing elit."
+        </p>
+        <p class="font-italic font-weight-normal mb-0">- Teresa May</p>
+      </div>
+      <div class="col-md-6 mb-0 mt-5">
+        <div class="d-flex justify-content-center mb-4">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp"
+            class="rounded-circle shadow-1-strong" width="100" height="100" />
+        </div>
+        <p class="lead my-3 ">
+          "Neque cupiditate assumenda in maiores repudiandae mollitia architecto elit sed
+          adipiscing elit."
+        </p>
+        <p class="font-italic font-weight-normal mb-0">- Teresa May</p>
+      </div>
+      <div class="col-md-6 mb-0 mt-5">
+        <div class="d-flex justify-content-center mb-4">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp"
+            class="rounded-circle shadow-1-strong" width="100" height="100" />
+        </div>
+        <p class="lead my-3 ">
+          "Neque cupiditate assumenda in maiores repudiandae mollitia architecto elit sed
+          adipiscing elit."
+        </p>
+        <p class="font-italic font-weight-normal mb-0">- Teresa May</p>
+      </div>
+      <div class="col-md-6 mb-0 mt-5">
+        <div class="d-flex justify-content-center mb-4">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp"
+            class="rounded-circle shadow-1-strong" width="100" height="100" />
+        </div>
+        <p class="lead my-3 ">
+          "Neque cupiditate assumenda in maiores repudiandae mollitia architecto elit sed
+          adipiscing elit."
+        </p>
+        <p class="font-italic font-weight-normal mb-0">- Teresa May</p>
+      </div>
+      <div class="col-md-6 mb-0 mt-5">
+        <div class="d-flex justify-content-center mb-4">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp"
+            class="rounded-circle shadow-1-strong" width="100" height="100" />
+        </div>
+        <p class="lead my-3 ">
+          "Neque cupiditate assumenda in maiores repudiandae mollitia architecto elit sed
+          adipiscing elit."
+        </p>
+        <p class="font-italic font-weight-normal mb-0">- Teresa May</p>
+      </div>
+    </div>
+  </section>
 </template>
--- src/views/ErroView.vue
+++ src/views/ErroView.vue
@@ -1,3 +1,8 @@
 <template>
+       <body class="d-flex flex-column justify-content-center align-items-center erro">
+    <h1><i class="fa-solid fa-triangle-exclamation"></i>Error 404! NOT FOUND</h1>
     
+    <RouterLink to="/" class="btn btn-dark">Return</RouterLink>
+
+</body>
 </template>
